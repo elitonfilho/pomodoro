c063d91b88d3f79303e3da6bfb2af728c033cfb3
Eliton <elitonfilho@gmail.com>
2020-01-29 23:35:39 -0300

    Initial commit

---
+++ handlePomodoro.py
@@ -0,0 +1,39 @@
+import time
+from datetime import date, timedelta
+from PyQt5.QtCore import QThread, pyqtSignal
+
+class HandlePomodoro(QThread):
+
+    valueChanged = pyqtSignal(int)
+
+    def __init__(self, parent=None):
+        super(HandlePomodoro, self).__init__(parent)
+        self.running = True
+        self.duration = timedelta(minutes=25)
+        self.today = date.today
+
+    def run(self):
+        while self.running:
+            print('thread id', int(QThread.currentThreadId()))
+            for i in range(1, 101):
+                print('value', i)
+                self.valueChanged.emit(i)
+                QThread.sleep(1)
+
+    def changeState(self):
+        self.running = ~self.running
+
+
+    # def tick(self):
+    #     self.duration = self.duration - timedelta(seconds=1)
+
+    # def run(self):
+    #     while self.running:
+    #         self.tick()
+    #         self.valueChanged.emit(self.duration.total_seconds())
+    #         QThread.sleep(1)
+
+
+if __name__ == "__main__":
+    pomodoro = HandlePomodoro()
+    pomodoro.run()
